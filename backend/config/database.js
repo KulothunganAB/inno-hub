1afddd0732d1cbdaacbb57ba2da9f9e33e0dbe8e
KulothunganAB <abkulothungan@gmail.com>
2026-02-03 11:43:20 +0530

    fix mysql connection for render

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,16 +1,14 @@
-const mysql = require('mysql2');
-require('dotenv').config();
+const mysql = require("mysql2/promise");
+require("dotenv").config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'innohub',
+  uri: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
 });
 
-const promisePool = pool.promise();
-
-module.exports = promisePool;
+module.exports = pool;
