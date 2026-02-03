503b8f18f72dd74c7dd6cefb6bcdc77663373009
KulothunganAB <abkulothungan@gmail.com>
2026-02-03 12:15:14 +0530

    fix mysql connection using DATABASE_URL

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,14 +1,10 @@
-const mysql = require("mysql2/promise");
-require("dotenv").config();
+const mysql = require('mysql2/promise');
+require('dotenv').config();
 
-const pool = mysql.createPool({
-  uri: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-});
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL is not defined');
+}
+
+const pool = mysql.createPool(process.env.DATABASE_URL);
 
 module.exports = pool;
